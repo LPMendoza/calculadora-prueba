d7a29a8ee2e0e46770f43990b2e57dd7c14d82f5
Alberto <ccastro0@ucol.mx>
2020-11-13 00:50:16 -0600

    Primer prueba

---
+++ calculadora.js
@@ -0,0 +1,115 @@
+var num1;
+var operador;
+var memoria = 0;
+var texto = document.getElementById('texto');
+
+function agregar1(){
+    var numero = document.getElementById('num1').value;
+    texto.innerHTML = texto.textContent+numero;
+}
+function agregar2(){
+    var numero = document.getElementById('num2').value;
+    texto.innerHTML = texto.textContent+numero;
+}
+function agregar3(){
+    var numero = document.getElementById('num3').value;
+    texto.innerHTML = texto.textContent+numero;
+}
+function agregar4(){
+    var numero = document.getElementById('num4').value;
+    texto.innerHTML = texto.textContent+numero;
+}
+function agregar5(){
+    var numero = document.getElementById('num5').value;
+    texto.innerHTML = texto.textContent+numero;
+}
+function agregar6(){
+    var numero = document.getElementById('num6').value;
+    texto.innerHTML = texto.textContent+numero;
+}
+function agregar7(){
+    var numero = document.getElementById('num7').value;
+    texto.innerHTML = texto.textContent+numero;
+}
+function agregar8(){
+    var numero = document.getElementById('num8').value;
+    texto.innerHTML = texto.textContent+numero;
+}
+function agregar9(){
+    var numero = document.getElementById('num9').value;
+    texto.innerHTML = texto.textContent+numero;
+}
+function agregar0(){
+    var numero = document.getElementById('num0').value;
+    texto.innerHTML = texto.textContent+numero;  
+}
+function decimal(){
+    var numero = document.getElementById('punto').value;
+    texto.innerHTML = texto.textContent+numero;  
+}
+function suma(){
+    num1 = texto.textContent;
+    operador = "+";
+    texto.innerHTML = "";
+}
+function resta(){
+    num1 = texto.textContent;
+    operador = "-";
+    texto.innerHTML = "";
+}
+function multiplicacion(){
+    num1 = texto.textContent;
+    operador = "*";
+    texto.innerHTML = "";
+}
+function division(){
+    num1 = texto.textContent;
+    operador = "/";
+    texto.innerHTML = "";
+}
+function reset(){
+    num1 = 0;
+    num2 = 0;
+    operador = "";
+    texto.innerHTML = "";
+}
+function resolver(){
+    var resultado = 0;
+
+    switch(operador){
+
+        case "+":
+            num2 = texto.textContent;
+            resultado = parseFloat(num1)+parseFloat(num2);
+            texto.innerHTML = resultado;
+            break;
+
+        case "-":
+            num2 = texto.textContent;
+            resultado = parseFloat(num1)-parseFloat(num2);
+            texto.innerHTML = resultado;
+            break;
+
+        case "*":
+            num2 = texto.textContent;
+            resultado = parseFloat(num1)*parseFloat(num2);
+            texto.innerHTML = resultado;
+            break;
+
+        case "/":
+            num2 = texto.textContent;
+            resultado = parseFloat(num1)/parseFloat(num2);
+            texto.innerHTML = resultado;
+            break;
+    } 
+}
+function agregarMemoria(){
+    memoria = texto.textContent;
+    texto.innerHTML = "";
+}
+function eliminarMemoria(){
+    memoria = 0;
+}
+function obtenerMemoria(){
+    texto.innerHTML = memoria;
+}
---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'v1_cache_calculadora_pwa',
+toCache = [
+    './',
+    './style.css',
+    './bootstrap.min.css',
+    './script.js',
+    './calculadora.js',
+]
+
+//Fase de instalacion de SW, almacena en cache los activos estaticos
+self.addEventListener('install',e=>{
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache =>{
+            return cache.addAll(toCache).then(() => self.skipWaiting())
+        })
+        .catch(error => console.log("Fallo el registro de cache",error))
+    )
+});
+
+//Activa el SW y busca los recursos para funcionar sin conexion
+self.addEventListener('activate',e=>{
+    const cacheWhitelist = [CACHE_NAME];
+    e.waitUntil(
+        caches.keys().then(cacheNombres =>{
+            cacheNombres.map(cacheNombre =>{
+                if(cacheWhitelist.indexOf(cacheNombre)===-1){
+                    return caches.delete(cacheNombre)
+                }
+            })
+        })
+        .then(() => self.clients.claim())
+    )
+});
+
+//Recupera una URL 
+self.addEventListener('fetch',e=>{
+    e.respondWith(
+        caches.match(e.request).then(res =>{
+            if(res){
+                return res;
+            }
+            return fetch(e.request);
+        })
+    )
+});
